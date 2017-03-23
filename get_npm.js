4a8d8e7bbf75ff55786a7ce5dfb192a00fd4c995
ztl <724236598@qq.com>
2017-03-23 11:24:41 +0800

    修改为淘宝镜像

--- get_npm.js
+++ get_npm.js
@@ -5,8 +5,12 @@ var util = require('util'),
 
 var NPM_PKG_JSON_URL = 'https://raw.githubusercontent.com/%s/%s/deps/npm/package.json';
 // https://github.com/npm/npm/tags
-var NVMW_NPM_MIRROR = process.env.NVMW_NPM_MIRROR || 'https://github.com/npm/npm/archive';
-var BASE_URL = NVMW_NPM_MIRROR + '/v%s.zip';
+//var NVMW_NPM_MIRROR = process.env.NVMW_NPM_MIRROR || 'https://github.com/npm/npm/archive';
+
+
+
+var NVMW_NPM_MIRROR = http: //npm.taobao.org/mirrors/npm
+    var BASE_URL = NVMW_NPM_MIRROR + '/v%s.zip';
 
 var targetDir = process.argv[2];
 var versions = process.argv[3].split('/');
@@ -14,60 +18,65 @@ var binType = versions[0];
 var binVersion = versions[1];
 
 if (binType === 'iojs') {
-  // detect npm version from https://iojs.org/dist/index.json
-  var NVMW_IOJS_ORG_MIRROR = process.env.NVMW_IOJS_ORG_MIRROR || 'https://iojs.org/dist';
-  var pkgUri = NVMW_IOJS_ORG_MIRROR + '/index.json';
-  wget(pkgUri, function (filename, content) {
-    if (filename === null) {
-      return noNpmAndExit();
-    }
-    var npmVersion;
-    var items = JSON.parse(content);
-    for (var i = 0; i < items.length; i++) {
-      var item = items[i];
-      if (!npmVersion) {
-        // make sure has a npm version
-        npmVersion = item.npm;
-      }
-      if (item.version === binVersion && item.npm) {
-        npmVersion = item.npm;
-        break;
-      }
-    }
+    // detect npm version from https://iojs.org/dist/index.json
+    var NVMW_IOJS_ORG_MIRROR = process.env.NVMW_IOJS_ORG_MIRROR || 'https://iojs.org/dist';
+    var pkgUri = NVMW_IOJS_ORG_MIRROR + '/index.json';
+    wget(pkgUri, function(filename, content) {
+        if (filename === null) {
+            return noNpmAndExit();
+        }
+        var npmVersion;
+        var items = JSON.parse(content);
+        for (var i = 0; i < items.length; i++) {
+            var item = items[i];
+            if (!npmVersion) {
+                // make sure has a npm version
+                npmVersion = item.npm;
+            }
+            if (item.version === binVersion && item.npm) {
+                npmVersion = item.npm;
+                break;
+            }
+        }
 
-    if (!npmVersion) {
-      return noNpmAndExit();
-    }
-    downloadNpmZip(npmVersion);
-  });
+        if (!npmVersion) {
+            return noNpmAndExit();
+        }
+        downloadNpmZip(npmVersion);
+    });
 } else {
-  var pkgUri = util.format(NPM_PKG_JSON_URL, 'joyent/node',
-    binVersion === 'latest' ? 'master' : binVersion);
-  wget(pkgUri, function (filename, pkg) {
-    if (filename === null) {
-      return noNpmAndExit();
-    }
-    downloadNpmZip(JSON.parse(pkg).version);
-  });
+    var pkgUri = "https://npm.taobao.org/mirrors/node/index.json";
+    wget(pkgUri, function(filename, pkg) {
+        if (filename === null) {
+            return noNpmAndExit();
+        }
+        var _pkg = JSON.parse(pkg);
+        for (var i = 0, n = _pkg.length; i < n; i++) {
+            var obj = _pkg[i];
+            if (obj.version == binVersion) {
+                downloadNpmZip(obj.npm);
+            }
+        }
+    });
 }
 
 function noNpmAndExit() {
-  console.error('%s %s does not include npm', binType, binVersion);
-  process.exit(1);
+    console.error('%s %s does not include npm', binType, binVersion);
+    process.exit(1);
 }
 
 function downloadNpmZip(version) {
-  var uri = util.format(BASE_URL, version);
-  wget(uri, function (filename, data) {
-    if (filename === null) {
-        console.error('Can\'t get npm: ' + uri);
-        process.exit(1);
-    }
-    fs.writeFile(path.join(targetDir, 'npm.zip'), data, function (err) {
-      if (err) {
-        return console.error(err.message);
-      }
-      console.log('Download npm %s is done', version);
+    var uri = util.format(BASE_URL, version);
+    wget(uri, function(filename, data) {
+        if (filename === null) {
+            console.error('Can\'t get npm: ' + uri);
+            process.exit(1);
+        }
+        fs.writeFile(path.join(targetDir, 'npm.zip'), data, function(err) {
+            if (err) {
+                return console.error(err.message);
+            }
+            console.log('Download npm %s is done', version);
+        });
     });
-  });
 }
--- nvmw.bat
+++ nvmw.bat
@@ -20,11 +20,11 @@ if %IS64% == TRUE (
 )
 
 if not defined NVMW_NODEJS_ORG_MIRROR (
-  set "NVMW_NODEJS_ORG_MIRROR=https://nodejs.org/dist"
+  set "NVMW_NODEJS_ORG_MIRROR=http://npm.taobao.org/mirrors/node"
 )
 
 if not defined NVMW_IOJS_ORG_MIRROR (
-  set "NVMW_IOJS_ORG_MIRROR=https://iojs.org/dist"
+  set "NVMW_IOJS_ORG_MIRROR=http://npm.taobao.org/mirrors/iojs"
 )
 
 if "%1" == "install" if not "%2" == "" (
@@ -134,7 +134,7 @@ if %NODE_TYPE% == iojs (
   if %ARCH% == x32 (
     set NODE_EXE_URL=%NVMW_NODEJS_ORG_MIRROR%/%NODE_VERSION%/node.exe
   ) else (
-    set NODE_EXE_URL=%NVMW_NODEJS_ORG_MIRROR%/%NODE_VERSION%/x64/node.exe
+    set NODE_EXE_URL=%NVMW_NODEJS_ORG_MIRROR%/%NODE_VERSION%/win-x64/node.exe
   )
 )
 
